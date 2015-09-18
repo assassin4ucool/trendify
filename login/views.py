ce9e1543717ab9c5dd3d5fddf1b55b927956895d
assassin4ucool <assassin4ucool@gmail.com>
2015-09-18 22:33:36 +0530

    changed the index page and the username added to get the username in index page

--- login/views.py
+++ login/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+	user = None
+	if request.user.is_authenticated():
+		user = request.user.username
+        
 	template = "index.html"
 	user = "amit"
 	context = {user:"user"}
